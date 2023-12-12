9107288db311d4dd11656ed5df3eaf4b4ba48a82
hyunkyoung <121487591+hymiel@users.noreply.github.com>
2023-12-12 20:29:07 +0900

    variable

---
+++ src/var/Var_Ex3.java
@@ -0,0 +1,11 @@
+package var;
+
+public class Var_Ex3 {
+    public static void main(String[] args) {
+        long l = 10000000000L;
+        System.out.println("l = " + l);
+        boolean b = true;
+        System.out.println("b = " + b);
+
+    }
+}
---
+++ src/var/Var_type.java
@@ -0,0 +1,25 @@
+package var;
+
+public class Var_type {
+
+    public  static void main(String[] args) {
+    int a = 100; //정수
+    double b = 10.5; //실수
+    boolean c = true; //true, false 입력 가능
+    char d = 'A'; //문자 하나
+    String e ="Hello Java"; //문자열, 문자열을 다루기위한 특별한 타입
+
+
+    //정수
+    byte f = 127; //-128~127
+    short g = 32767; // -32,768 ~ 32,767
+    int h = 2147483647; //-2,147,483,648 ~ 2,147,483,647 (약 20억)
+
+    //-9,223,372,036,854,775,808 ~ 9,223,372,036,854,775,807
+     long i = 9223372036775807L;
+
+    //실수
+     float j = 10.0f;
+     double k = 10.0;
+         }
+}
