4095e8b44c8fc6a571cd116f8c0051bef495ff36
hyunkyoung <121487591+hymiel@users.noreply.github.com>
2023-12-12 20:36:33 +0900

    loop continue

---
+++ src/loop/Continue1.java
@@ -0,0 +1,18 @@
+package loop;
+
+public class Continue1 {
+    public static void main(String[] args) {
+        //1부터 5까지 숫자를 출력하는데 숫자가 3일 때는 출력을 건너뛸 것
+
+        int i = 1;
+        while (i <= 5) {
+             if (i == 3) {
+                 i++;
+                 continue;
+             }
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
