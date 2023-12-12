92b3bd0a5e0be6cdcd6000b16be11e32a56e0c05
hyunkyoung <121487591+hymiel@users.noreply.github.com>
2023-12-12 20:36:09 +0900

    loop for

---
+++ src/loop/For1.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class For1 {
+    //for문 구조
+//    for (1.초기식; 2.조건식; 4.증감식;) {
+//        3.코드;
+//    }
+
+    public static void main(String[] args) {
+        //1부터10까지 출력하는 for문
+        for (int i = 1; i <=10; i++) {
+            System.out.println(i);
+        }
+    }
+}
