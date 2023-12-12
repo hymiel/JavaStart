7140c45590cfbbd1a9601c04a02bfcd12aeb3ca8
hyunkyoung <121487591+hymiel@users.noreply.github.com>
2023-12-12 20:37:05 +0900

    loop  break

---
+++ src/loop/Brack2.java
@@ -0,0 +1,19 @@
+package loop;
+
+public class Brack2 {
+    public static void main(String[] args) {
+
+        //for문으로 풀어보자
+        //1부터 시작하여 숫자를 계속 누적해서 더하다가 합계가 10보다 큰 처음 값은?
+        int sum = 0;
+        int i = 1;
+
+        for (;;) { // 조건식이 없으면 무한반복
+            sum = sum + i;
+            if (sum > 10) {
+                System.out.println("합이 10보다 크면 종료 : i =" + i + ", sum =" + sum);
+                break;
+            }  i++;
+        }
+    }
+}
---
+++ src/loop/Break_continue.java
@@ -0,0 +1,21 @@
+package loop;
+
+public class Break_continue {
+    //break : 반복문을 즉시 종료
+    //continue : 반복문의 나머지를 건너 뛰고 다음 반복문으로 진행
+//    public static void main(String[] args) {
+//        //break
+//        while (조건식) {
+//            코드 1;
+//            break; // 즉시 while문 종료
+//            코드 2;
+//        }
+//
+//        //continue
+//        while (조건식) {
+//            코드1;
+//            continue; //코드2를 실행하지 않고 즉시 조건식으로 이동
+//            코드2;
+//        }
+//    }
+}
