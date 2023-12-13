0fe5a2e21f42c80200fe66e1b787b4bf3987386b
hyunkyoung <121487591+hymiel@users.noreply.github.com>
2023-12-13 14:53:15 +0900

    배열의 변수 선언+생성

---
+++ src/array/Array1.java
@@ -0,0 +1,19 @@
+package array;
+
+public class Array1 {
+    public static void main(String[] args) {
+        //학생의 점수를 출력하는 프로그램
+//        int[] student; // 배열 변수 선언
+//        student = new int[5]; //배열 생성
+
+//        int student[] = new int[5]; //배열의 변수 선언+생성 방법1
+        int[] student = new int[5]; //배열의 변수 선언+생성 방법2
+
+        //변수값 대입
+        student[0] = 90;
+        student[1] = 80;
+        student[2] = 70;
+        student[3] = 60;
+        student[4] = 50;
+    }
+}
