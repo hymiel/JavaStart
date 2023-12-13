55962b9b7114c5dbb3539b56aae4b9608c76973b
hyunkyoung <121487591+hymiel@users.noreply.github.com>
2023-12-13 14:42:06 +0900

    키오스크 - 상품 주문

---
+++ src/scanner/Scanner_Ex9.java
@@ -0,0 +1,42 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Scanner_Ex9 {
+    public static void main(String[] args) {
+        //상품 구매
+        Scanner input = new Scanner(System.in);
+        int totalCost = 0;
+
+        while (true) {
+            System.out.println("1 : 상품입력, 2 : 결제, 3 : 프로그램 종료");
+            int option = input.nextInt();
+
+            if (option == 1){
+                input.nextLine();
+
+                System.out.println("상품명을 입력하세요.");
+                String product = input.nextLine();
+
+                System.out.println("상품의 가격을 입력하세요.");
+                int place = input.nextInt();
+
+                System.out.println("상품의 구매 수량을 입력하세요.");
+                int quantity = input.nextInt();
+
+                totalCost += place * quantity;
+
+                System.out.println("상품명 : " + product + ", 가격 : " + place + ", 수량 : " + quantity + ", 합계 : " + place * quantity);
+
+            } else if (option == 2) {
+                System.out.println("총 비용 : " + totalCost);
+                totalCost = 0;
+            } else if (option == 3) {
+                System.out.println("프로그램을 종료합니다.");
+            } else {
+                System.out.println("올바른 옵션을 선택해주세요,");
+            }
+
+        }
+    }
+}
